0e265744c240e3345b540e00b2aa026cbf229c04
trishaDas13 <trisha.das1308@gmail.com>
2023-11-05 12:09:03 +0530

    home page done

---
+++ script.js
@@ -0,0 +1,27 @@
+let animationArea = document.querySelector('.animationArea');
+
+//todo: -------- Fetch API --------
+async function fetchAPI(){
+    let data = await fetch('https://api.coingecko.com/api/v3/search/trending');
+    let res = await data.json();
+    let bitcoin = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr');
+    let price = await bitcoin.json();
+    appendCrypto(res, price);
+}
+fetchAPI();
+//todo: -------- append crypto --------
+function appendCrypto(res, price) {
+    let bitcoin = price.bitcoin.inr;
+    res.coins.forEach(ele => {
+        let priceCard = document.createElement('div');
+        priceCard.classList.add('priceCard');
+        priceCard.innerHTML = `
+            <img src="${ele.item.large}" alt="">
+            <div class="cardContent">
+                <h1>${ele.item.name}</h1>
+                <p>${(ele.item.price_btc * bitcoin)}</p>
+            </div>
+        `
+        animationArea.appendChild(priceCard);
+    });
+}
